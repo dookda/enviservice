e7bae535dab928880197d17f43885c8e44bdf4db
dookda <sakda.homhuan@gmail.com>
2022-02-22 11:01:22 +0700

    update

--- www/app.js
+++ www/app.js
@@ -2,3 +2,15 @@ const d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").innerText = year;
 console.log(year);
+
+
+let quest = [5, 6, 7, 8, 9];
+
+quest.map(i => {
+    let tagetDiv = document.getElementById("quest")
+    let lstQuest = `<div class="shadow-none p-3 mb-1 bg-light rounded flex" role="alert">
+สอบถาม คำถาม ปัญหา <button class="btn btn-info" style="margin-left: auto;">รายละเอียด</button>
+</div>`;
+
+    tagetDiv.innerHTML += lstQuest;
+})
