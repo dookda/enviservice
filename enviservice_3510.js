d080faf857eb5d1d89d4aacf56765bf3bc5ea95c
dookda <sakda.homhuan@gmail.com>
2022-04-05 09:07:03 +0700

    update button logout

--- enviservice_3510.js
+++ enviservice_3510.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 // app.options('*', cors());
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3510');
+    res.setHeader('Access-Control-Allow-Origin', 'https://envirservice.co.th');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
